702f2d8991bb40032c56e8c79e2c2c33d3c2ffeb
Surendra234 <101715947+Surendra234@users.noreply.github.com>
2022-03-27 16:48:47 +0530

    Add files via upload

---
+++ harmonic.sh
@@ -0,0 +1,11 @@
+echo "enter a number"
+
+read n
+i=1
+
+for (( n=1; n<=10; n++ ))
+do
+	echo $i/$n ;
+done
+
+
---
+++ power.sh
@@ -0,0 +1,13 @@
+echo "enter a number :"
+
+read n
+
+
+for (( i=2;i<=n;i++ ))
+
+do
+
+		echo  = $(( i^n )) ;
+done
+
+
